746c521f0c7a9aaf6e7b8b7693efc46841cc0f78
athira <athira.ks@chordify.com>
2023-02-21 18:13:35 +0530

    prime checking

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 
 puts "Enter the number:"
 num=gets.chomp.to_i
-count=0
+flag=false
 if (num==0)
 	puts "0 is not prime"
 else
@@ -10,13 +10,13 @@ else
 	i=2
 	while(i<num)
 		if (num%i==0)
-			count+=1
+			flag=true
 		end
 		i+=1
 	end
 	
 end
-	if count>1
+	if flag
 		puts "#{num} is not a prime number"
 	else
 		puts "#{num} is a prime number"
