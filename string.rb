5e7436dd86fa871ca053d07ee9217edcb7ad70e6
athira <athira.ks@chordify.com>
2023-02-21 14:28:27 +0530

    string program

---
+++ string.rb
@@ -0,0 +1,8 @@
+str="rubyonrails"
+length=str.length
+puts "length of the string is #{length}"
+if length > 8
+puts "Eligible for password creation"
+else
+puts "Not eligible for password creation"	
+end
