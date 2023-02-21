d1515c477e3f1b1387f26e1e4ce673451529756c
athira <athira.ks@chordify.com>
2023-02-21 18:14:39 +0530

    fibonacci series

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,14 +5,12 @@ nextnum=0
 puts "Enter the number of terms:-"
 n=gets.chomp.to_i
 count=1
+puts "#{first}"
 while(count<=n+1)
-    if(count<=1)
-        nextnum=count
-    else
+
         puts nextnum
         nextnum=first+second
         first=second
         second=nextnum
-    end
     count+=1
 end
