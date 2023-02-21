a8014c20c77bba6e564cc0a6845fc2df3634fa55
athira <athira.ks@chordify.com>
2023-02-21 15:48:23 +0530

    prime number

---
+++ prime.rb
@@ -0,0 +1,17 @@
+num=10
+count = 0
+if num == 0 || num== 1
+	puts "not prime number"
+else
+	for i in 2..num do
+		if num%i == 0
+			count = 1
+			break
+		end
+	end
+if count== 0
+	puts "Prime number"
+else
+	puts "Not a prime number"
+	end
+end
