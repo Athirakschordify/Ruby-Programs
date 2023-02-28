f759e8d10a5f85ffe55a35322eacb910737b897f
athira <athira.ks@chordify.com>
2023-02-28 09:44:57 +0530

    array 1

--- arr1.rb
+++ arr1.rb
@@ -1,9 +1,18 @@
 
-numbers=[1,2,3,4,5,6]
+puts "Enter size of the array"
+n=gets.chomp.to_i
+array=[]
+puts "Enter array elements"
+for i in 1..n do
+    num=gets.chomp.to_i
+    array.push(num)
+end
+puts "array is #{array}"
+
 sum=0
-for i in numbers
-	if i %2 ==0
-		sum=sum+i
-	end	
-end	
+for i in array
+if i %2 ==0
+sum=sum+i
+end
+end
 puts sum
