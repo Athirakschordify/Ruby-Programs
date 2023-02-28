b1c9b35335694f7e29641fb7fa93fef146297890
athira <athira.ks@chordify.com>
2023-02-28 09:46:26 +0530

    hash 2

--- hash2.rb
+++ hash2.rb
@@ -2,32 +2,19 @@ attendance_data = [
 
   { "stage"=> "Main Stage", "attendance"=> 5000 },
 
-  { "stage"=> "Rock Stage", "attendance"=> 3000},
+  { "stage"=> "Rock Stage", "attendance"=> 8000},
 
   { "stage"=> "Dance Stage", "attendance"=> 7000 },
 
-  { "stage"=> "Acoustic Stage", "attendance"=> 2000 }, 
+  { "stage"=> "Acoustic Stage", "attendance"=> 2000 },
 
 ]
-
-
-
-
-
 maximum=0
-stage_name=""
+stage_name=''
 attendance_data.each do |item|
-  puts maximum
-  if item['attendance'].to_i > maximum.to_i
-    puts "-------------------"
-    maximum= item['attendance']
-    stage_name=item['stage_name']
-  else
-   maximum= item['attendance']
-  end  
-
-end 
-
-  
-puts " #{stage_name}  had the highest attendance with #{maximum} people"
-
+    if item["attendance"] >maximum
+      maximum=item['attendance']
+      stage_name=item['stage']
+    end  
+end    
+puts "#{stage_name} had the highest attendance with #{maximum} people"
